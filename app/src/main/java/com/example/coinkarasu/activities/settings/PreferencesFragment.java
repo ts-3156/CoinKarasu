3ac8a0de95679d3533ec3e4a53d38182bb7c0a6d
ts-3156 <ts_3156@yahoo.co.jp>
2017-12-21 13:52:14 +0900

    Show open source software licenses

--- app/src/main/java/com/example/coinkarasu/activities/settings/PreferencesFragment.java
+++ app/src/main/java/com/example/coinkarasu/activities/settings/PreferencesFragment.java
@@ -1,13 +1,17 @@
 package com.example.coinkarasu.activities.settings;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 
 import com.example.coinkarasu.R;
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 
 public class PreferencesFragment extends PreferenceFragment {
 
@@ -17,6 +21,30 @@ public class PreferencesFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.fragment_preferences);
+
+        Preference prefAppVersion = getPreferenceScreen().findPreference("pref_app_version");
+        prefAppVersion.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(R.string.pref_app_version_up_to_date)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+                builder.create().show();
+
+                return true;
+            }
+        });
+
+        Preference prefOpenSourceLicenses = getPreferenceScreen().findPreference("pref_open_source_licenses");
+        prefOpenSourceLicenses.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent(getActivity(), OssLicensesMenuActivity.class));
+                return true;
+            }
+        });
+
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         bindPreferenceSummaryToValue(prefs, "pref_sync_frequency");
